46586044880e3afb5419238ae420b26e58ba3b05
Kadi Kraman <hellokadi@gmail.com>
2024-08-03 22:16:57 -0500

    Configure react-native/no-unused-styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: ["expo", "prettier"],
-  plugins: ["prettier"],
+  plugins: ["prettier", "react-native"],
   rules: {
     "prettier/prettier": "error",
+    "react-native/no-unused-styles": "error",
   },
 };

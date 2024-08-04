d66627c915af47b57e55d17d75b5072debacbd4e
Kadi Kraman <hellokadi@gmail.com>
2024-08-04 06:05:31 -0500

    Example of stack navigation with expo router

---
+++ app/counter.tsx
@@ -0,0 +1,28 @@
+import { useRouter } from "expo-router";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+
+export default function CounterScreen() {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => router.navigate("/idea")}>
+        <Text style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}>
+          Go to /idea
+        </Text>
+      </TouchableOpacity>
+      <Text style={styles.text}>Counter</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+  },
+});

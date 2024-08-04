cd655452e621616827166fbd0b5bb7dc1ed67959
Kadi Kraman <hellokadi@gmail.com>
2024-08-04 06:22:26 -0500

    Example of presentation: modal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,14 @@ export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Shopping list" }} />
+      <Stack.Screen
+        name="counter"
+        options={{ title: "Counter", presentation: "modal" }}
+      />
+      <Stack.Screen
+        name="idea"
+        options={{ title: "My idea", presentation: "modal" }}
+      />
     </Stack>
   );
 }
